b8bfb21b312e403cf1ab1e0b918c36d17abd55e6
TheRipSon <61383871+TheRipSon@users.noreply.github.com>
2022-05-15 14:20:57 +0200

    pointery

--- libMain.c
+++ libMain.c
@@ -1,8 +1,46 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
 
-int main(int argc,char ** argv){
+
+pthread_mutex_t mutex;
 
 
 
-	return 0;
+void* czytelnik(void *arg) {
+    int *i = arg;
+    printf("%d\n",*i);
+    free(i);
+}
+
+
+int main(int argc, char* argv[]) {
+    pthread_t p1,p2;
+    pthread_mutex_init(&mutex, NULL);
+    int * x = malloc(sizeof(int));
+    *x=2;
+
+
+    if (pthread_create(&p1, NULL, &czytelnik, x) != 0) {
+        free(x);
+        return 2;
+    }
+    x = malloc(sizeof(int));
+    *x=3;
+
+    if (pthread_create(&p2, NULL, &czytelnik, x) != 0) {
+        free(x);
+        return 2;
+    }
+    if (pthread_join(p1, NULL) != 0) {
+        return 5;
+    }
+
+    if (pthread_join(p2, NULL) != 0) {
+        return 5;
+    }
+
+    pthread_mutex_destroy(&mutex);
+    return 0;
 }
