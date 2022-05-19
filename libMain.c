7eee43536f429895e860e164dc6031f847c72f74
TheRipSon <61383871+TheRipSon@users.noreply.github.com>
2022-05-19 22:34:43 +0200

    fine

--- libMain.c
+++ libMain.c
@@ -3,38 +3,45 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
-
-struct node {
-	pthread_t * thread;
-	struct node * next;
-};
+#include <time.h>
 
 
 pthread_mutex_t mutex;
 
+
 void* reader(void *arg) {
-	int	*i = arg;
-    printf("%d\n",*i);
+    int r = 1000000+rand() % 500000;
+	int	i = *(int*)arg;
+    printf("%d\n",i);
+    usleep(r);
 }
 
 void* writer(void *arg) {
-	int	*i = arg;
-    printf("%d\n",*i);
+    pthread_mutex_lock(&mutex);
+    int r = 1000000+rand() % 500000;
+    int	i = *(int*)arg;
+    printf("%d\n",i);
+    usleep(r);
+    pthread_mutex_unlock(&mutex);
 }
 
 int addReader(int * a,pthread_t *p){
+    int c=*a;
 	if (pthread_create(p, NULL, &reader, a) != 0) {
+        free(a);
         return -1;
     }
 	pthread_join(*p,NULL);
-	return 0;
+	return c;
 }
 int addWriter(int * a,pthread_t *p){
+    int c=*a;
 	if (pthread_create(p, NULL, &writer, a) != 0) {
+        free(a);
         return -1;
     }
 	pthread_join(*p,NULL);
-	return 0;
+	return c;
 }
 
 int main(int argc, char* argv[]) {
@@ -42,46 +49,38 @@ int main(int argc, char* argv[]) {
 	int W = atoi(argv[1]);
 	int R = atoi(argv[2]);
     pthread_mutex_init(&mutex, NULL);
-	pthread_t ** tab_W = (pthread_t **)malloc(sizeof(pthread_t*)*W);
-	pthread_t ** tab_R = (pthread_t **)malloc(sizeof(pthread_t*)*R);
+	pthread_t ** tab = (pthread_t **)malloc(sizeof(pthread_t*)*(W+R));
 	int i=0;
-	for(i=0;i<W;i++){
-		tab_W[i]=(pthread_t *)malloc(sizeof(pthread_t));
-	}
-	for(i=0;i<R;i++){
-		tab_R[i]=(pthread_t *)malloc(sizeof(pthread_t));
-	}
-
-	int ** c = (int**)malloc(sizeof(int*)*(W+R));
 	for(i=0;i<W+R;i++){
-		c[i]=(int *)malloc(sizeof(int));
-		*c[i]=i;
+		tab[i]=(pthread_t *)malloc(sizeof(pthread_t));
 	}
+    int *a=(int *)malloc(sizeof(int));
+    int z=0;
+    int * kolejka = (int*)malloc(sizeof(int)*(W+R));
+    int * koniec = NULL,* poczatek = NULL;
+    koniec = &kolejka[0];
+    poczatek = &kolejka[W+R-1];
+    for(i=0;i<W+R;i++){
+        kolejka[i]=i;
+    }
+    while(i<10){
+        for(poczatek;poczatek==koniec;poczatek++)
+        {
+            if(poczatek==kolejka[W+R-1])
+                poczatek=kolejka[0];
 
-	addReader(c[0],tab_R[0]);
-	addReader(c[1],tab_R[1]);
-	addReader(c[2],tab_R[2]);
-	sleep(2);
-	printf("ASDASDSAD\n");
 
 
 
-
-
+        }
+        i++;
+    }
 
 
 	for(i=0;i<W+R;i++){
-		free(c[i]);
+		free(tab[i]);
 	}
-	free(c);
-	for(i=0;i<W;i++){
-		free(tab_W[i]);
-	}
-	for(i=0;i<R;i++){
-		free(tab_R[i]);
-	}
-	free(tab_W);
-	free(tab_R);
+	free(tab);
 
 
     pthread_mutex_destroy(&mutex);
